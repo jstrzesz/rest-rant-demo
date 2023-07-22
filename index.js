6161181efb312eb72162f03f716007b9971ab122
Josh S <josh@operationspark.org>
2023-07-22 14:22:11 -0500

    adding 404 page route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 
+const { PORT } = process.env;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('app is listening on port 3000');
+app.get('*', (req, res) => {
+  res.send('<h1>404 Page</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
 });
